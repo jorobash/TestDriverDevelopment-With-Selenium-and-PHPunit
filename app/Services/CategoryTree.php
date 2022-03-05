d52c4d86fe74b38aa2eedb84c6428b66c1fa4d7c
joro <jorkomzx@gmail.com>
2022-03-05 10:45:45 +0200

    test nested level of menu on one level

--- app/Services/CategoryTree.php
+++ app/Services/CategoryTree.php
@@ -9,7 +9,15 @@ class CategoryTree
         $nested_categories = array();
         foreach($db_array as $key => $category){
             $category['children'] = [];
-            $nested_categories[] = $category;
+            if($category['parent_id'] == 1){
+                $key_of_parent = array_search(1,array_column($db_array,'id'));
+                $key_of_child = $key;
+                $db_array[$key_of_child]['children'] = [];
+                $nested_categories[$key_of_parent]['children'][] = $db_array[$key_of_child];
+            }else{
+                $nested_categories[] = $category;
+            }
+
         }
 
         return $nested_categories;
--- tests/CategoryTreeTest.php
+++ tests/CategoryTreeTest.php
@@ -7,17 +7,45 @@ class CategoryTreeTest extends \PHPUnit\Framework\TestCase
         $tree = new \App\Services\CategoryTree();
 
         $db_result = [
-          ['id' => 1, 'name' => 'Electronics','parent_id' => null],
-          ['id' => 2, 'name' => 'Videos', 'parent_id' => null],
-          ['id' => 3, 'name' => 'Software','parent_id' => null]
+            ['id' => 1, 'name' => 'Electronics', 'parent_id' => null],
+            ['id' => 2, 'name' => 'Videos', 'parent_id' => null],
+            ['id' => 3, 'name' => 'Software', 'parent_id' => null]
         ];
 
         $after_conversion = [
-          ['id' => 1, 'name' => 'Electronics','parent_id' => null,'children' => []],
-          ['id' => 2, 'name' => 'Videos', 'parent_id' => null,'children' => []],
-          ['id' => 3, 'name' => 'Software','parent_id' =>null,'children' => []]
+            ['id' => 1, 'name' => 'Electronics', 'parent_id' => null, 'children' => []],
+            ['id' => 2, 'name' => 'Videos', 'parent_id' => null, 'children' => []],
+            ['id' => 3, 'name' => 'Software', 'parent_id' => null, 'children' => []]
         ];
 
-        $this->assertEquals($after_conversion,$tree->convert($db_result));
+        $this->assertEquals($after_conversion, $tree->convert($db_result));
+    }
+
+    public function testCanConvertDatabaseResultToOneLevelNestedArray()
+    {
+        $tree = new \App\Services\CategoryTree();
+
+        $db_result = [
+            ['id' => 1, 'name' => 'Electronics', 'parent_id' => null],
+            ['id' => 2, 'name' => 'Computers', 'parent_id' => 1]
+        ];
+
+        $after_conversion = [
+            [
+                'id' => 1,
+                'name' => 'Electronics',
+                'parent_id' => null,
+                'children' => [
+                    [
+                        'id' => 2,
+                        'name' => 'Computers',
+                        'parent_id' => 1,
+                        'children' => []
+                    ]
+                ]
+            ]
+        ];
+
+        $this->assertEquals($after_conversion, $tree->convert($db_result));
     }
 }
